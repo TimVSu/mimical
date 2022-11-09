41a27b914fa97be89b74eba8e53ddc390fb7d929
Stoil Iliev <siliev@smail.uni-koeln.de>
2022-11-09 18:17:31 +0100

    Added basic notifications

--- pages/home.js
+++ pages/home.js
@@ -1,31 +1,32 @@
-import { Button } from 'react-native'
-import { Center} from 'native-base'
-
+import { Button } from "react-native";
+import { Center } from "native-base";
 
 const Home = ({ navigation }) => {
-    return (
+  return (
     <>
-    <Center mt="10">
-      <Button mt="5"
-        title="login"
-        onPress={() =>
-          navigation.navigate('Login')
-        }
-      />
-    </Center>
-    <Center mt="10">
-      <Button mt="5"
-        title="cam Preview"
-        onPress={() =>
-          navigation.navigate('Level')
-        }
-      />
-    </Center>
+      <Center mt="10">
+        <Button
+          mt="5"
+          title="login"
+          onPress={() => navigation.navigate("Login")}
+        />
+      </Center>
+      <Center mt="10">
+        <Button
+          mt="5"
+          title="cam Preview"
+          onPress={() => navigation.navigate("Level")}
+        />
+      </Center>
+      <Center mt="10">
+        <Button
+          mt="5"
+          title="Notifications"
+          onPress={() => navigation.navigate("Notifications")}
+        />
+      </Center>
     </>
-    );
-  };
+  );
+};
 
 export default Home;
-
-
-
--- stacks/rootStack.js
+++ stacks/rootStack.js
@@ -1,26 +1,25 @@
-import Login from '../pages/login.js'
-import Home from '../pages/home.js'
-import * as React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import LevelLayout from '../components/levelLayout.js'
-
+import Login from "../pages/login.js";
+import Home from "../pages/home.js";
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import LevelLayout from "../components/levelLayout.js";
+import Notification from "../pages/notifications.js";
 
 const Stack = createNativeStackNavigator();
 
 const RootStack = () => {
   return (
-    
-      <Stack.Navigator>
-        <Stack.Screen
-          name="Home"
-          component={Home}
-          options={{ title: 'Welcome' }}
-        />
-        <Stack.Screen name="Login" component={Login} />
-        <Stack.Screen name='Level' component={LevelLayout} />
-      </Stack.Navigator>
-    
+    <Stack.Navigator>
+      <Stack.Screen
+        name="Home"
+        component={Home}
+        options={{ title: "Welcome" }}
+      />
+      <Stack.Screen name="Login" component={Login} />
+      <Stack.Screen name="Level" component={LevelLayout} />
+      <Stack.Screen name="Notifications" component={Notification} />
+    </Stack.Navigator>
   );
 };
 
