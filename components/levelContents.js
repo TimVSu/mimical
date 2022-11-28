891b38559997f70a7385a5e7ab0eb4bfdf6af2f9
Tim Suchan <tsuchan@smail.uni-koeln.de>
2022-11-28 19:51:04 +0100

    permission now works on android

---
+++ components/levelContents.js
@@ -0,0 +1,45 @@
+
+/*enum Tags{
+    NOSE,
+    CHEEKS,
+    EYES,
+    LIPS,
+    MOUTH
+
+}
+
+class taskContent{
+
+    constructor(id, tags,text, description){
+        this.id = id;
+        this.tags = tags;
+        this.description = description
+        this.text = text;
+    }
+
+    getDescription(){
+        return this.description;
+    }
+    getTags(){
+        return this.tags;
+    }
+    getId(){
+        return this.id;
+    }
+}
+
+const taskContent1 = new taskContent(1, [Tags.CHEEKS, Tags.NOSE],
+     ["Es ist Samstagmorgen und Sie ziehen heute in Ihre neue Wohnung ein. Noch ein wenig verschlafen," +
+ "laufen Sie durch Ihre alte Wohnung. Sie gehen in Gedanken noch einmal die Umzugsplanung für den heutigen Tag durch.",
+  "Mit den Handflächen reiben Sie sich das Gesicht ab."],
+  "Reiben sie ihr Gesicht mit den Handflächen");
+
+const taskContent2 = new taskContent(2, [Tags.LIPS, Tags.MOUTH], ["Alle Kartons sind gepackt und stehen bereit." +
+ "Auch das Bett ist bereits abgebaut und umzugsbereit. Ihr Blick schweift durch die Wohnung auf das schwere," +
+ "sperrige Sofa. Sie fragen sich, ob es durch den engen Flur passen wird.", "Besorgt ziehen Sie die Augenbrauen hoch."],
+  "ziehen sie die Augenbrauen hoch" );*/
+
+  
+
+
+
