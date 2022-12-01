9eb48556015c47464ec605e986f8cd3b15770fb8
Maxim Torgovitski <mtorgovi@smail.uni-koeln.de>
2022-12-01 18:56:11 +0100

    switch added to settings_item

--- components/settings_item.js
+++ components/settings_item.js
@@ -1,7 +1,7 @@
 // author: Maxim Torgovitski
 
 // import react native
-import { Button, ScrollView, StyleSheet, Text, View } from 'react-native';
+import { Button, ScrollView, StyleSheet, Switch, Text, useColorScheme, View } from 'react-native';
 import React, { useState } from 'react';
 
 // import components
@@ -16,15 +16,27 @@ import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
 // import { faBell } from '@fortawesome/free-solid-svg-icons';
 
 // colors
+const green = 'rgb(52, 199, 89)';
 const gray5 = 'rgb(229, 229, 234)';
 
 // return settings item component
 const SettingsItem = (props) => {
+  const colorScheme = useColorScheme();
+  const containerColor = colorScheme === 'light' ? styles.light_container : styles.dark_container;
+  const textColor = colorScheme === 'light' ? styles.light_text : styles.dark_text;
+  const [isEnabled, setIsEnabled] = useState(false);
+  const toggleSwitch = () => setIsEnabled(previousState => !previousState);
   return (
-    <View style={styles.settings_item}>
+    <View style={[styles.settings_item, containerColor]}>
       {/* <FontAwesomeIcon icon={props.icon} size={32} /> */}
-      <Text style={[styles.label, { marginTop: 4 }]}>{props.label}</Text>
-      <FontAwesomeIcon icon={props.toggle} size={32} color={props.toggle_color} />
+      <Text style={[styles.label, textColor]}>{props.label}</Text>
+      <Switch
+        trackColor={{ false: "#767577", true: green }}
+        thumbColor={isEnabled ? "#f4f3f4" : "#f4f3f4"}
+        // ios_backgroundColor={"#3e3e3e"}
+        onValueChange={toggleSwitch}
+        value={isEnabled}
+      />
     </View>
   );
 }
