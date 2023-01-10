8fdfc984dc6111dd58e00dcfff9246f342989880
Maxim Torgovitski <mtorgovi@smail.uni-koeln.de>
2023-01-10 16:03:13 +0100

    home update

--- pages/home.js
+++ pages/home.js
@@ -1,28 +1,14 @@
 // authors: Tim Suchan, Maxim Torgovitski
 
 // import react native
-import { ScrollView, Text, useColorScheme, View } from "react-native";
+import { ScrollView, Text, useColorScheme, View, } from "react-native";
 
 // import components
 import Button from "../components/button";
 import styles from "../components/styles";
 
 // import icons
-import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
-import { faHouse } from "@fortawesome/free-solid-svg-icons";
-import { faPlay } from "@fortawesome/free-solid-svg-icons";
-import { faUser } from "@fortawesome/free-solid-svg-icons";
-import { faCamera } from "@fortawesome/free-solid-svg-icons";
-import { faGear } from "@fortawesome/free-solid-svg-icons";
-import { faBell } from "@fortawesome/free-solid-svg-icons";
-import { faCalendar } from "@fortawesome/free-solid-svg-icons";
-
-// colors
-const green = "rgb(52, 199, 89)";
-const blue = "rgb(0, 122, 255)";
-const gray4 = "rgb(209, 209, 214)";
-const gray5 = "rgb(229, 229, 234)";
-const gray6 = "rgb(242, 242, 247)";
+import { faBell, faCalendar, faCamera, faChartSimple, faGear, faHouse, faPlay, faUser } from "@fortawesome/free-solid-svg-icons";
 
 // return home page
 const Home = ({ navigation }) => {
@@ -34,40 +20,27 @@ const Home = ({ navigation }) => {
 
   return (
     <View style={[{ flex: 1 }, containerColor]}>
-      <View style={[{ flex: 1 }, containerColor, { justifyContent: "center" }, { alignItems: "center" },]}      >
-        <View style={[{ backgroundColor: gray6 }, { width: 256 }, { height: 256 }, { borderRadius: 16 }, { borderBottomColor: gray4 }, { padding: 16 }, { margin: 8 }, { justifyContent: "flex-end" }, squareColor,]}        >
-          <Text style={[{ fontSize: 32 }, { color: gray4 }, { opacity: 0.5 }, textColor,]}>Szenario</Text>
-          <Text style={[{ fontSize: 16 }, { color: gray4 }, { opacity: 0.5 }, textColor,]}>Übung</Text>
+      <View style={[{ flex: 1 }, containerColor, { justifyContent: "center" }, { alignItems: "center" },]}>
+        <View style={[squareColor, { width: 256 }, { height: 256 }, { borderRadius: 16 }, { padding: 16 }, { margin: 8 }, { justifyContent: "flex-end" }]}>
+          <Text style={[{ fontSize: 32 }, textColor, { opacity: 0.5 }]}>Szenario</Text>
+          <Text style={[{ fontSize: 16 }, textColor, { opacity: 0.5 }]}>Übung</Text>
         </View>
-        <Button
-          icon={faPlay}
-          label="Szenario fortsetzen"
-          navigation={navigation}
-          target={"Menu"}
-        />
-        <Button
-          icon={faHouse}
-          label="Übersicht"
-          navigation={navigation}
-          target={"Menu"}
-        />
+        <View>
+          <Button icon={faPlay} label="Szenario fortsetzen" navigation={navigation} target={"Menu"} />
+          <Button icon={faHouse} label="Übersicht" navigation={navigation} target={"Menu"} />
+          <Button icon={faChartSimple} label="Fortschritt" navigation={navigation} target={"Progress"} />
+          <Button icon={faGear} label="Einstellungen" navigation={navigation} target={"Settings"} />
+        </View>
       </View>
-      <View style={[{ borderTopWidth: 1 }, { width: "100%" }, containerColor]}>
+      {/* <View style={[{ borderTopWidth: 1 }, { width: "100%" }, containerColor]}>
         <ScrollView>
-          {/* <Button
-            icon={faUser}
-            label="Log in"
-            navigation={navigation}
-            target={"Login"}
-          /> */}
-          <Button
-            icon={faUser}
-            label="Log in"
-            navigation={navigation}
-            target={"HomeLogin"}
-          />
+          <Button icon={faUser} label="Log in" navigation={navigation} target={"Login"} />
+          <Button icon={faUser} label="Log in" navigation={navigation} target={"HomeL"} />
+          <Button icon={faCamera} label="Cam Preview" navigation={navigation} target={"Level"} />
+          <Button icon={faBell} label="Notifications" navigation={navigation} target={"Notifications"} />
+          <Button icon={faCalendar} label="Calendar" navigation={navigation} target={"Calendar"} />
         </ScrollView>
-      </View>
+      </View> */}
     </View>
   );
 
